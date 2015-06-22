93a2673e2c5434a4a5b875fe74171648f01e3d9a
Luke Iannini <lukexi@me.com>
2015-06-21 18:28:09 -0700

    Remove extraneous do

--- exec/main.hs
+++ exec/main.hs
@@ -2,16 +2,15 @@ import Halive
 import Banner
 import System.Environment
 
-separateArgs args = do
-  let (haliveArgs, targetArgs) = break (=="--") args
-  in  (haliveArgs, drop 1 targetArgs)
+separateArgs :: [String] -> ([String], [String])
+separateArgs args = (haliveArgs, drop 1 targetArgs)
+  where (haliveArgs, targetArgs) = break (== "--") args
 
 main :: IO ()
 main = do
-    (args, targetArgs) <- separateArgs <$> getArgs
-    print targetArgs
-    case args of
-        [] -> putStrLn "Usage: halive <main.hs> <include dir> [-- <args to myapp>]"
-        (mainName:includeDirs) -> do
-            putStrLn banner
-            withArgs targetArgs $ recompiler mainName includeDirs
+  (args, targetArgs) <- separateArgs <$> getArgs
+  case args of
+    [] -> putStrLn "Usage: halive <main.hs> <include dir> [-- <args to myapp>]"
+    (mainName:includeDirs) -> do
+      putStrLn banner
+      withArgs targetArgs $ recompiler mainName includeDirs
